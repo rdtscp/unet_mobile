5c1aa92a2554f74ebff47a0cd5f31a2c1ff2fb83
Alexander Wilson <acwilson96@gmail.com>
2018-08-02 15:38:39 +0100

    Unfinished Changes

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,4 @@
+import GiftedMessenger from 'react-native-gifted-messenger'
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -9,24 +10,70 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Dimensions,
+  Picker,
+} from 'react-native'
 
 export default class unet_mobile extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <View style={{
+            flex: 1,
+            marginTop: 20,
+          }}>
+          <GiftedMessenger
+            ref={(c) => this._GiftedMessenger = c}
+            styles={{
+              container: {
+                width: Dimensions.get('window').width,
+              },
+              bubbleRight: {
+                marginLeft: 70,
+                backgroundColor: "#007aff",
+              },
+            }}
+            autoFocus={false}
+            messages={[
+              {
+                text: 'Are you building a chat app!?',
+                name: 'React-Bot',
+                image: {uri: 'https://facebook.github.io/react/img/logo_og.png'},
+                position: 'left',
+                date: new Date(2016, 3, 14, 13, 0),
+                uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+              },
+              {
+                text: "Yes, and I use Gifted Messenger!",
+                name: 'Awesome Developer',
+                image: null,
+                position: 'right',
+                date: new Date(2016, 3, 14, 13, 1),
+                uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+              },
+            ]}
+            handleSend={() => {}}
+            onErrorButtonPress={() => {}}
+            maxHeight={Dimensions.get('window').height - 20}
+        
+            loadEarlierMessagesButton={false}
+            onLoadEarlierMessages={() => {}}
+        
+            senderName='Awesome Developer'
+            senderImage={null}
+            onImagePress={() => {}}
+            displayNames={true}
+        
+            parseText={true}
+            handlePhonePress={() => {}}
+            handleUrlPress={() => {}}
+            handleEmailPress={() => {}}
+        
+            isLoadingEarlierMessages={false}
+        
+            typingMessage={""}
+          />
+        </View>
     );
   }
 }
